15e3c4124e66483ca4caeca6ca2e1199dffabad8
Dzambulov Arman <arman1407@ya.ru>
2023-05-29 17:50:15 +0300

    task#27

---
+++ task#27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+string number = Console.ReadLine()!;
+int summa = 0;
+for (int i = 0; i < number.Length; i++)
+summa = summa + Convert.ToInt32(number[i].ToString());
+Console.WriteLine(summa);
+
+
+
+
+
+
+
+
+
